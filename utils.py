d63358e3a53d7ea8354dfb6839a149c0b5dfd4ff
Liuzycn <liuzy0158@gmail.com>
2025-03-10 09:57:20 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import json
 import time
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-21839635f9f44970bddcd1f4fca8f674", base_url="https://api.deepseek.com")  #sk-f38e6a38790d47cfb38ce1cc6446e570
+client = OpenAI(api_key="<your api key>", base_url="https://api.deepseek.com")  
 
 def read_jsonl(file_path):
     _data = []
